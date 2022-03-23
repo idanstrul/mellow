f70066c75b1db8e550cdd67483d8f08df2c40dc8
idanstr <strulovich@mail.tau.ac.il>
2022-03-23 10:29:52 +0200

    Progressin task edit

--- src/views/task-details.vue
+++ src/views/task-details.vue
@@ -1,8 +1,15 @@
 <template>
   <section class="task-details modal-screen">
       <div class="modal-container">
+        <div v-if="false" class="cover"></div>
         <div class="modal-header">I'm a task details modal</div>
-        <div class="modal-main"></div>
+        <div class="modal-main">
+          <div v-if="false" class="notations"></div>
+          <div class="description"></div>
+          <div v-if="false" class="attachments"></div>
+          <div v-if="false" class="checklists"></div>
+          <div class="activity-log"></div>
+        </div>
         <div class="modal-sidebar">
           <button>Join</button>
           <span class="action-group">Add to card</span>
