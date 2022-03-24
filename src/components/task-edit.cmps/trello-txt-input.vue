4d3812a0ab45972cfb74c79a3876439aab9cbacf
idanstr <strulovich@mail.tau.ac.il>
2022-03-24 17:00:42 +0200

    Progress in text edit

--- src/components/task-edit.cmps/trello-txt-input.vue
+++ src/components/task-edit.cmps/trello-txt-input.vue
@@ -2,9 +2,9 @@
     <section class="trello-txt-inpput" @click="isInEdit = true">
         <span v-if="!isInEdit" class="todo-title">{{ updatedTxt }}</span>
         <div v-else class="todo-edit">
-            <textarea cols="30" rows="3" v-model="updatedTxt"></textarea>
-            <button @click="saveTxt">Save</button>
-            <button @click="cancel">X</button>
+            <textarea cols="30" rows="3" v-model="updatedTxt" placeholder="Write somthingr"></textarea>
+            <button @click.stop="saveTxt">Save</button>
+            <button @click.stop="cancel">X</button>
         </div>
     </section>
 </template>
@@ -20,16 +20,17 @@ export default {
             updatedTxt: this.txt,
             isInEdit: false
         }
-    }, 
+    },
     methods: {
         saveTxt() {
+            if (!this.updatedTxt) return this.$emit('txtIsEmpty')
             this.$emit('txtSaved', this.updatedTxt)
             this.isInEdit = false
         },
         cancel() {
             this.updatedTxt = this.txt
             this.isInEdit = false
-        }
+        },
     }
 }
 </script>
