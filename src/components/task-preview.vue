d0d0bfbddbef5c7fa827f48d45d769a5e6bf6934
AmitMi1 <98426263+AmitMi1@users.noreply.github.com>
2022-03-24 16:24:35 +0200

    task members

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -1,10 +1,15 @@
 <template>
 <section class="task-preview">
 <p>{{ task.title }}</p>
+<section v-if="task.members" class="task-members">
+  <user-avatar v-for="m in task.members" :key="m._id" :user="m"></user-avatar>
+</section>
 </section>
 </template>
 
 <script>
+
+import userAvatar from "./user-avatar.vue"
 
 export default {
   name: 'task-preview',
@@ -12,7 +17,7 @@ export default {
     task: Object,
   },
   components: {
-    // customCard,
+    userAvatar
   },
   methods: {
     goToDetail() {
