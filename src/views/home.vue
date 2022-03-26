f94ce964792b2598eaafb679e4c6bb7287049382
Omer Eshkoli <omere653@gmail.com>
2022-03-26 21:47:19 +0300

    homepage

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,31 +1,39 @@
 <template>
-  <section>
-    <!-- <board-details></board-details> -->
-    <h1>Home page</h1>
-  <nav class="btn-container">
-      <router-link to="/login">Login</router-link>|
-      <router-link to="/signup">Signup</router-link>
-    </nav>
-  </section>
+  <div class="homepage">
+     <section class="header-home">
+      <div class="logo">
+         <span class="material-icons-outlined logo-icon"></span>Mellow
+      </div>
+      <nav class="homepage-nav">
+         <router-link to='/login' class="login">Log in</router-link>
+         <span> | </span>
+         <router-link to='/signup' class="signup">Sign up</router-link>
+      </nav>
+    </section>
+    <div class="layout">
+    <main class="main-home-page">
+    <section class="started">
+       <p class="txt-title"> Administer your strategy Collaborate with coworkers and friends unquestionably and effectively</p>
+      <button class="start-btn" @click="goToBoardsPage">Get Started</button>
+    </section>
+
+   
+</main>
+</div>
+  </div>
 </template>
 
 <script>
-
-import boardDetails from "./board-details.vue"
-
 export default {
   name: 'home',
-  data() {
-    return {
-
-    }
-  },
   components: {
-    boardDetails
-  }
-}
+  },
+  created() {},
+  methods: {
+    async goToBoardsPage() {
+      await this.$store.dispatch({ type: "goToBoardsPage" });
+      this.$router.push("/board/b101");
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
