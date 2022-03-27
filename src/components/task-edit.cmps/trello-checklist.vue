a91847de48581f51ef762bebd2c470d7294c4410
idanstr <strulovich@mail.tau.ac.il>
2022-03-27 14:43:40 +0300

    Progress in checklists

--- src/components/task-edit.cmps/trello-checklist.vue
+++ src/components/task-edit.cmps/trello-checklist.vue
@@ -4,23 +4,31 @@
             class="section-title text-m icon-checklist"
             :txt="updatedChecklist.title"
             @txt-saved="updateChecklistTitle"
+            deny-empty-save
         ></trello-txt-input>
         <button class="btn float-right">Delete</button>
-        <span class="progress-count"></span>
-        <el-progress :percentage="completePercentage" :status="passSuccess"/>
-        <div class="progress-bar"></div>
+        <!-- <span class="progress-count"></span> -->
+        <el-progress :percentage="completePercentage" :status="passSuccess" />
+        <!-- <div class="progress-bar"></div> -->
         <ul class="todos clean-list">
             <li v-for="(todo, todoIdx) in updatedChecklist.todos" :key="todo.id">
                 <input type="checkbox" v-model="todo.isDone" />
                 <trello-txt-input
-                    class="inline-block"
                     :txt="todo.title"
                     @txt-saved="updateTodoTitle($event, todo.id)"
                     @txt-is-empty="removeTodo(todoIdx)"
                 ></trello-txt-input>
             </li>
         </ul>
-        <button class="btn">Add an item</button>
+        <trello-txt-input
+            class="make-btn"
+            placeholder="Add an item"
+            deny-empty-save
+            reset-on-save
+            @txt-saved="addTodo"
+            @enter-clicked="next()"
+            ref="input"
+        ></trello-txt-input>
         <!-- <pre>{{ updatedChecklist }}</pre> -->
     </section>
 </template>
@@ -39,30 +47,53 @@ export default {
         }
     },
     methods: {
+        updateChecklistTitle(updatedTitle) {
+            this.updatedChecklist.title = updatedTitle
+        },
         updateTodoTitle(updatedTitle, todoId) {
             console.log('updatedTitle', updatedTitle);
             // this.$emit('updated', this.updatedDesc)
             const idx = this.updatedChecklist.todos.findIndex(todo => todo.id === todoId)
             this.updatedChecklist.todos[idx].title = updatedTitle
         },
-        updateChecklistTitle(updatedTitle) {
-            this.updatedChecklist.title = updatedTitle
-        },
         removeTodo(todoIdx) {
             this.updatedChecklist.todos.splice(todoIdx, 1)
         },
+        addTodo(title) {
+            this.updatedChecklist.todos.push({
+                id: this.makeId(),
+                title,
+                isDone: false
+            })
+        },
         cancel() {
             this.updatedDesc = this.description
+        },
+        makeId(length = 8) {
+            var txt = '';
+            var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            for (var i = 0; i < length; i++) {
+                txt += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return txt;
+        },
+        next(){
+            // console.log('event',event);
+            // console.log(this.$refs.input.$el);
+            this.$refs.input.$el.click()
+            // event.target.click()
         }
+
     },
     computed: {
         completePercentage() {
             const todos = this.updatedChecklist.todos
             const total = todos.length;
+            if (total === 0) return 0
             const completed = todos.filter(todo => todo.isDone).length
-            return (completed / total) * 100
+            return Math.floor((completed / total) * 100)
         },
-        passSuccess(){
+        passSuccess() {
             if (this.completePercentage === 100) return 'success'
             return ''
         }
--- src/components/task-edit.cmps/trello-txt-input.vue
+++ src/components/task-edit.cmps/trello-txt-input.vue
@@ -2,7 +2,7 @@
     <section class="trello-txt-input" @click="isInEdit = true">
         <span v-if="!isInEdit" class="input-title">{{ txtToShow }}</span>
         <div v-else class="input-edit">
-            <textarea cols="30" rows="3" v-model="updatedTxt" :placeholder="placeholder" autofocus></textarea>
+            <textarea cols="30" rows="3" @keydown.enter.prevent="next" v-model="updatedTxt" :placeholder="placeholder" v-focus></textarea>
             <button class="primary-btn" @click.stop="saveTxt">Save</button>
             <button class="cancel-btn" @click.stop="cancel"></button>
         </div>
@@ -17,6 +17,13 @@ export default {
         placeholder: {
             type: String,
             default: ''
+        },
+        denyEmptySave: {
+            type: Boolean,
+            default: false
+        },
+        resetOnSave: {
+            type: Boolean
         }
     },
     data() {
@@ -28,16 +35,22 @@ export default {
     methods: {
         saveTxt() {
             if (!this.updatedTxt) {
-                if (this.placeholder) this.isInEdit = false
+                console.log('this.denyEmptySave', this.denyEmptySave);
+                if (!this.denyEmptySave) this.isInEdit = false
                 return this.$emit('txtIsEmpty', '')
             }
             this.$emit('txtSaved', this.updatedTxt)
+            if (this.resetOnSave) this.updatedTxt = '';
             this.isInEdit = false
         },
         cancel() {
             this.updatedTxt = this.txt
             this.isInEdit = false
         },
+        next(){
+            this.saveTxt()
+            if (!this.isInEdit) this.$emit('enterClicked')
+        }
     },
     computed: {
         txtToShow() {
