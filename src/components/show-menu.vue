3640e92dc01bee279e61e19c3466f63b85d1da08
Omer Eshkoli <omere653@gmail.com>
2022-03-28 12:37:32 +0300

    show menu

--- src/components/show-menu.vue
+++ src/components/show-menu.vue
@@ -3,7 +3,16 @@
     <div>
       <Transition enter-active-class="animate__animated animate__slideInRight"
       leave-active-class="animate__animated animate__slideOutLeft">
-        <div v-if="menuOpen" class="custom-menu-container"></div>
+        <div v-if="menuOpen" class="custom-menu-container">
+             <span class="btn-boards-drop">Change background</span>
+             <li
+            v-for="color in colors"
+            :key="color"
+            :style="{ backgroundColor: color }"
+            class="color"
+            @click="changeBackground(color, '')"
+          ></li>
+        </div>
       </Transition>
     </div>
    </div>
@@ -18,11 +27,24 @@ export default {
     return {
     }
   },
+  computed: {
+    background() {
+       (this.board.style["background-color"])
+        return { "background-color": this.board.style["background-color"] };
+    },
   methods: {
       close() {
       this.$emit("close");
     },
+    
+changeBackground(color) {
+      const style = {
+        "background-color": color,
+      };
+      this.$emit("setBackground", style);
+    },
   }
+}
 }
 </script>
 
