4671f8d8e00f5947c7eaf389afcd4f9523251906
Omer Eshkoli <omere653@gmail.com>
2022-03-28 11:58:39 +0300

    show-menu

---
+++ src/components/show-menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="side-menu-popup">
+    <div>
+      <Transition enter-active-class="animate__animated animate__slideInRight">
+<div v-if="menuOpen" class="custom-menu-container"></div>
+      </Transition>
+   </div>
+   </div>
+</template>
+
+<script>
+export default {
+    props: {
+    menuOpen:Boolean,
+    },
+  data(){
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
