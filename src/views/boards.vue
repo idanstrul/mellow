9a08ad5685f527608945021687de334741e97c43
Omer Eshkoli <omere653@gmail.com>
2022-04-01 01:33:57 +0300

    .

--- src/views/boards.vue
+++ src/views/boards.vue
@@ -34,44 +34,14 @@ export default {
     openBoard(boardId) {
       this.$router.push(`/board/${boardId}`);
     },
-    boardTitleToShow(boardTitle) {
-      if (boardTitle.length > 40) return boardTitle.substring(0, 40) + "...";
-      else return boardTitle;
-    },
     closeAddBoard() {
       this.isAddBoard = false;
     },
-    async addBoard(board) {
-      try {
-        const newBoard = await this.$store.dispatch({
-          type: "saveBoard",
-          board,
-        });
-        this.$store.commit({ type: "setCurrBoard", board: newBoard });
-        this.backgroundColor = board.style["background-color"];
-        this.backgroundImg = board.style["background-image"];
-        const activity = {
-          txt: "created this board",
-          byMember: this.$store.getters.getMyMiniUser,
-        };
-        await this.$store.dispatch({
-          type: "addActivity",
-          activity,
-          boardId: newBoard._id,
-        });
-        this.$router.push(`/b/${newBoard._id}`);
-      } catch (err) {
-        console.log("ERROR cannot add board");
-      }
     },
-  },
   computed: {
     boards() {
       return this.$store.getters.boards;
     },
-    classToBoardPage(){
-       return {'isLessThen3Boards':this.boards.length <= 6,'isMoreThen3Boards': this.boards.length > 6}
-    }
   },
 };
 </script>
