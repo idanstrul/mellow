aa3b4d6a1de9bc3ebc6f7a1ba21f8120d4dfbffc
AmitMi1 <98426263+AmitMi1@users.noreply.github.com>
2022-04-04 20:50:56 +0300

    .

--- src/views/home.vue
+++ src/views/home.vue
@@ -14,7 +14,7 @@
         <h1>Mello helps teams move work forward.</h1>
         <p>As your software team grows, Mello makes it easy to manage workflows across multiple teams and projects. Simple, intuitive, fast: Mello is where software teams do their best work.
          Free to get started, with easy user experience Mello.</p>
-        <router-link to='/board/624afa5e47cdb81018c1bc99' class="btn-start">Start as a guest</router-link>
+        <router-link to='/board/624b2ee347cdb81018c1bc9a' class="btn-start">Start as a guest</router-link>
         </div>
         <!-- <button class="start-btn" @click="goToBoardsPage">Start as a guest</button> -->
       </main>
