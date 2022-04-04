20b6b9d51e9587a4d69461a9510ee7c25983953d
Omer Eshkoli <omere653@gmail.com>
2022-04-04 10:21:56 +0300

    .

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,31 +1,32 @@
 <template>
-<section class="home">
-  <div class="homepage">
-     <section class="header-home">
-      <div class="logo">
-         <span></span>Mello
-      </div>
-      <nav class="homepage-nav">
-         <router-link to='/login' class="login">Log in</router-link>
-         <span> | </span>
-         <router-link to='/signup' class="signup">Sign up</router-link>
-      </nav>
-    </section>
-    <div class="layout">
-    <main class="main-home-page">
-    <section class="started">
-       <p class="txt-title"> 
-        <h1>Mello helpss teams move work forward</h1>
-         As your software team grows, Mello makes it easy to manage workflows across multiple teams and projects. Simple, intuitive, fast: Mello is where software teams do their best work.
-         Free to get started, with easy user experience Mello. </p>
-      <button class="start-btn" @click="goToBoardsPage">Start as a guest</button>
-    </section>
-        <div class="img">
-          <img src="../assets/Asset3.png">
+  <section class="home">
+    <div class="homepage">
+      <section class="header-home">
+        <div class="logo">
+          <span></span>Mello
         </div>
-</main>
-</div>
-  </div>
+        <nav class="homepage-nav">
+          <router-link to='/login' class="login">Log in</router-link>
+          <span> | </span>
+          <router-link to='/signup' class="signup">Sign up</router-link>
+        </nav>
+      </section>
+      <div class="layout">
+        <main class="main-home-page">
+          <section class="started">
+            <p class="txt-title">
+            <h1>Mello helps teams move work forward</h1>
+            As your software team grows, Mello makes it easy to manage workflows across multiple teams and projects.
+            Simple, intuitive, fast: Mello is where software teams do their best work.
+            Free to get started, with easy user experience Mello. </p>
+            <button class="start-btn" @click="goToBoardsPage">Start as a guest</button>
+          </section>
+          <div class="img">
+            <img src="../assets/Asset3.png">
+          </div>
+        </main>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -34,7 +35,7 @@ export default {
   name: 'home',
   components: {
   },
-  created() {},
+  created() { },
   methods: {
     async goToBoardsPage() {
       // await this.$store.dispatch({ type: "goToBoardsPage" });
