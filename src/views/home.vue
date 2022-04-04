019411ad018d13731bb949b4af0409fd349a2399
AmitMi1 <98426263+AmitMi1@users.noreply.github.com>
2022-04-04 15:28:38 +0300

    .

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,23 +1,36 @@
 <template>
-<section class="home">
-  <div class="homepage">
+<section class="homepage">
+      <nav class="homepage-nav flex">
+         <router-link title="Boards" to='/board' class="logo"><img src="../assets/homeLogo.png" alt=""></router-link>
+        <div>
+
+         <router-link title="Login" to='/login' class="login">Log in</router-link>
+         <router-link title="Signup" to='/signup' class="signup">Sign up</router-link>
+        </div>
+      </nav>
+      <main class="flex center">
+        <img class="hero" src="../assets/Asset3.png" alt="">
+        <div class="text">
+        <h1>Mello helps teams move work forward.</h1>
+        <p>As your software team grows, Mello makes it easy to manage workflows across multiple teams and projects. Simple, intuitive, fast: Mello is where software teams do their best work.
+         Free to get started, with easy user experience Mello.</p>
+        <router-link to='/board/6249680f207696d8ae3ee216' class="btn-start">Start as a guest</router-link>
+        </div>
+        <!-- <button class="start-btn" @click="goToBoardsPage">Start as a guest</button> -->
+      </main>
+  <!-- <div class="homepage">
      <section class="header-home">
       <div class="logo">
          <span></span>Mello
       </div>
-      <nav class="homepage-nav">
-         <router-link to='/login' class="login">Log in</router-link>
-         <span> | </span>
-         <router-link to='/signup' class="signup">Sign up</router-link>
-      </nav>
     </section>
     <div class="layout">
     <main class="main-home-page">
     <section class="started">
-       <p class="txt-title"> 
         <h1>Mello helpss teams move work forward</h1>
+       <p class="txt-title"> 
          As your software team grows, Mello makes it easy to manage workflows across multiple teams and projects. Simple, intuitive, fast: Mello is where software teams do their best work.
-         Free to get started, with easy user experience Mello. </p>
+         Free to get started, with easy user experience Mello.</p>
       <button class="start-btn" @click="goToBoardsPage">Start as a guest</button>
     </section>
         <div class="img">
@@ -25,7 +38,7 @@
         </div>
 </main>
 </div>
-  </div>
+  </div> -->
   </section>
 </template>
 
